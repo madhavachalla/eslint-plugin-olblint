8a94509c9ecd7c15d7a68da803304c46e03155a2
ankit vishnoi <ankit.vishnoi@kony.com>
2018-05-17 00:35:11 +0530

    Added rules no-mvc-navigation and no-timers

---
+++ tests/lib/rules/no-mvc-navigation.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Do not use mvc navigation method
+ * @author Ankit
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-mvc-navigation"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-mvc-navigation", rule, {
+    
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "var ntf = new kony.mvc.Navigation('frmAccountsDetails');",
+            errors: [{
+                message: "Avoid using kony MVC Navigation Methods",
+                type: "MemberExpression"
+            }]
+        },
+        {
+            code: "var ntf = new konyNameSpece.Navigation('frmAccountsDetails');",
+            errors: [{
+                message: "Avoid using kony MVC Navigation Methods",
+                type: "MemberExpression"
+            }]
+        }
+    ]
+});
