e56a830743d3b516434461e7e92129898df200ee
KITSPL\kh9296 <shivam.marwaha@kony.com>
2018-06-18 18:44:01 +0530

    Require JSDOC will check protypal decalarations which is missed by eslint default rule

--- tests/lib/rules/require-jsdoc.js
+++ tests/lib/rules/require-jsdoc.js
@@ -21,63 +21,53 @@ var ruleTester = new RuleTester();
 ruleTester.run("require-jsdoc", rule, {
 
     valid: [
-        "/*@param @param @return*/Transfer_PresentationController.prototype.repeatTransfer = function (transactionObject,onBackPressed) {var self = this;}",
-        "/*@return*/function x () {}",
-       '/*@return*/function doSomething() {return 1+2;}',
-        '/*@param @return*/function doSomething(string) {return string;}',
-        '/*@param @return*/var test = function(string){console.log("test");}',
-        'var comp = React.createClass({/*@return*/render: function(){}});',
-        'module.exports = function(){return true};'
+        `
+        /**
+         * Description
+         */
+        PC.prototype.sample = function () {
+
+        }
+        `,
+        `
+        PC.protoxtype.sample = function () {
+
+        }
+        `
     ],
 
     invalid: [
-        {
-            code: 'var outerFunction = function(){/*@return*/function innerFunction(){}}',
-            errors: [ { message: 'Missing @jsdoc for outerFunction' }]
-        },
-        {
-            code: 'function doSomething() {return 1+2;}',
-            errors: [ { message: 'Missing @jsdoc for doSomething' } ]
-        },
-        {
-            code: 'function doSomething(string) {return string}',
-            errors: [ { message: 'Missing @jsdoc for doSomething' } ]
-        },
-        {
-            code: '/*@return*/function doSomething(string) {return string}',
-            errors: [ { message: 'Missing @param description in the jsdoc for doSomething' } ]
-        },
-        {
-            code: '/*@param*/function doSomething(string) {return string}',
-            errors: [ { message: 'Missing @return description in the jsdoc for doSomething' } ]
-        },
-        {
-            code: 'var test = function(){console.log("test");}',
-            errors: [ { message: 'Missing @jsdoc for test' } ]
-        },
-        {
-            code: 'var test = function(string){console.log("test");}',
-            errors: [ { message: 'Missing @jsdoc for test' } ]
-        },
-        {
-            code: '/*@return*/var test = function(string){console.log(string);}',
-            errors: [ { message: 'Missing @param description in the jsdoc for test' } ]
-        },
-        {
-            code: '/*@param*/var test = function(string){console.log(string);}',
-            errors: [ { message: 'Missing @return description in the jsdoc for test' } ]
-        },
-        {
-            code: '/*@param*/var test = function(string,string2){console.log(string);}',
-            errors: [ {message: 'Missing @return description in the jsdoc for test'}, { message: 'Missing @param description in the jsdoc for test' } ]
-        },
-        {
-            code: '/*@param@return*/var test = function(var1, var2){console.log(string);}',
-            errors: [ { message: 'Missing @param description in the jsdoc for test' } ]
-        },
-        {
-            code: 'var comp = React.createClass({render: function(){}});',
-            errors: [ { message: 'Missing @jsdoc for render' } ]
+      {
+          code:  
+          `
+          PC.prototype.sample = function () {
+  
+          }
+          `,
+          errors: [
+              {
+                  type: "FunctionExpression",
+                  message: 'Missing JSDoc comment.'
+              }
+          ]
+      },
+      {
+        code:  
+        `
+        /**
+         * Description
+         */
+        
+        PC.prototype.sample = function () {
+
         }
+        `,
+        errors: [
+            {
+                type: "FunctionExpression",
+                message: 'Missing JSDoc comment.'
+            }
+        ]
+    }
     ]
 });
